def25ab2df85518b041a69ab8b45186d7644ffaa
Dakota <dakotat103@gmail.com>
2024-01-21 01:11:11 -0700

    Camera damping ver 0.1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -25,10 +25,20 @@ public class CameraController : MonoBehaviour
     public float minZoom = 5f;
     public float maxZoom = 20f;
 
+    //Seperating out the target forward vector and the acutal forward vector allows us to smoothdamp the rotation values
+    //without losing which direction we should be facing for position calculation purposes
+    Vector3 virtualForward = Vector3.zero;
+
     //This will give a unit vector that tells the direction the camera is facing with a y of 0
     public Vector3 camOrientation
     {
         get { return transform.forward.With(y: 0).normalized; }
+    }
+
+    //The same as above but for the virtualForwardVector
+    public Vector3 virtualCamOrientation
+    {
+        get { return virtualForward.With(y: 0).normalized;}
     }
 
     public float initialXRotation = 60;
@@ -36,8 +46,12 @@ public class CameraController : MonoBehaviour
     public float maxXRot = 70;
     public float minXRot = 30;
 
+    Vector3 Velocity = Vector3.zero;
+    [SerializeField] private float smoothTime = 0.25f;
+
     void Awake()
     {
+        virtualForward = transform.forward;
         tryTargetObject(selectedTarget);
     }
 
@@ -45,8 +59,27 @@ public class CameraController : MonoBehaviour
     {
         targetPos = selectedTarget.transform.position;
 
-        transform.position = CalculateCameraPosition();
-        transform.rotation = Quaternion.LookRotation(targetPos-transform.position, Vector3.up);
+
+        trySetRotation(Quaternion.LookRotation(targetPos - transform.position, Vector3.up));
+    }
+
+    void LateUpdate()
+    {
+        
+
+
+        transform.position = Vector3.SmoothDamp(transform.position, CalculateCameraPosition(), ref Velocity, smoothTime);
+    }
+
+    //Use this to set the camera's rotation, so that we can make sure the camera doesn't snap to unwanted angles
+    bool trySetRotation(Quaternion newRotation)
+    {
+        //TODO:
+        //Find a better way to do this
+        
+
+        transform.rotation = newRotation;
+        return true;
     }
 
     public void Zoom(float amount)
@@ -58,7 +91,15 @@ public class CameraController : MonoBehaviour
     public void RotateX(float amount)
     {
         localXRotation += amount;
-        localXRotation = Mathf.Clamp(localXRotation, minXRot, maxXRot);
+        if (minXRot > localXRotation || localXRotation > maxXRot)
+        {
+            localXRotation = Mathf.Clamp(localXRotation, minXRot, maxXRot);
+        } else
+        {
+            Quaternion virtualRotation = Quaternion.AngleAxis(amount, Vector3.right);
+            virtualForward = virtualRotation * virtualForward;
+        }
+        
     }
 
     //Instead of using several functions to control camera transform in parallel,
@@ -67,7 +108,7 @@ public class CameraController : MonoBehaviour
     Vector3 CalculateCameraPosition()
     {
         //First -- According to the camZoom level, calculate the position of the camera if it was facing the origin with 0 rotation (as if it was on the XZ plane)
-        Vector3 unrotatedCamPosOrigin = -camOrientation * camZoom;
+        Vector3 unrotatedCamPosOrigin = -virtualCamOrientation * camZoom;
 
         //Construct our rotator with the camera's local x axis
         Quaternion targetRotator = Quaternion.AngleAxis(localXRotation, transform.right);
